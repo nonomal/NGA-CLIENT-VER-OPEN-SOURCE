11803d023c14108db8671a4b7056106f649e7a40
Justwen <yyhshrrr@126.com>
2023-07-28 20:46:54 +0800

    修复发帖提升失败的问题

--- nga_phone_base_3.0/src/main/java/sp/phone/task/TopicPostTask.java
+++ nga_phone_base_3.0/src/main/java/sp/phone/task/TopicPostTask.java
@@ -24,9 +24,9 @@ public class TopicPostTask extends AsyncTask<String, Integer, String> {
 
     private static final String LOG_TAG = TopicPostTask.class.getSimpleName();
 
-    private final String result_start_tag = "<span style='color:#aaa'>&gt;</span>";
+    private final String result_start_tag = "<title>";
 
-    private final String result_end_tag = "<br/>";
+    private final String result_end_tag = "</title>";
 
     private String mReplyUrl = Utils.getNGAHost() + "post.php?";
 
