686b86d709b5da637d730e42c4470d3eda945b9c
Justwen <yyhshrrr@126.com>
2025-02-07 21:19:54 +0800

    短消息发送改造为compose实现

--- lib_base_service_api/src/main/java/com/justwen/androidnga/base/activity/ARouterConstants.kt
+++ lib_base_service_api/src/main/java/com/justwen/androidnga/base/activity/ARouterConstants.kt
@@ -5,7 +5,7 @@ class ARouterConstants {
         const val ACTIVITY_MESSAGE_LIST = "/message/list"
         const val ACTIVITY_MESSAGE_LIST_NAME =
             "com.justwen.androidnga.module.message.compose.MessageListActivity"
-        const val ACTIVITY_MESSAGE_POST = "/activity/post"
+        const val ACTIVITY_MESSAGE_POST = "/message/post"
         const val ACTIVITY_MESSAGE_DETAIL = "/message/detail"
 
     }
